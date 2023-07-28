1a0229879b216a6514eb24b66d66449a67278238
MissMo101 <mohamedmajida577@gmail.com>
2023-07-28 14:31:14 -0700

    Update2

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -10,10 +10,16 @@
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
+	list_t *new_node;
+	unsigned int len = 0;
+
+	while (str[len])
+		len++;
+
 	if (str == NULL)
 		return (NULL);
 
-	list_t *new_node = malloc(sizeof(list_t));
+	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
 		return (NULL);
 
