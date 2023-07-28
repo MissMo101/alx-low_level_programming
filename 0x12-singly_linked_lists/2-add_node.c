691dc7897d684172672dad108916bcd28ee39ede
MissMo101 <mohamedmajida577@gmail.com>
2023-07-28 14:25:04 -0700

    Update

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -10,10 +10,15 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
+	list_t *new_node;
+	unsigned int len = 0;
+
+	while (str[len])
+		len++;
+
 	if (str == NULL)
 		return (NULL);
 
-	list_t *new_node;
 	
 	new_node = malloc(sizeof(list_t));
 
@@ -30,5 +35,5 @@ list_t *add_node(list_t **head, const char *str)
 	new_node->next = *head;
 	*head = new_node;
 
-	return (new_node);
+	return (*head);
 }
