d0cc088fae67fb41945b81aad9f44fb7a83371d7
Majida Mohamed <133633166+MissMo101@users.noreply.github.com>
2023-08-02 20:45:59 +0200

    Update 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -16,37 +16,30 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int i;
 
 	new_node = malloc(sizeof(listint_t));
-	if (head == NULL)
+	if (!new_node || !head)
 		return (NULL);
+
+	new_node->n = n;
+	new_node->next = NULL;
 
 	if (idx == 0)
 	{
-		new_node = malloc(sizeof(listint_t));
-		if (new_node == NULL)
-			return (NULL);
-
-		new_node->n = n;
 		new_node->next = *head;
 		*head = new_node;
 		return (new_node);
 	}
 
-	for (i = 0; i < idx - 1; i++)
+	for (i = 0; temp && i < idx; i++)
 	{
-		if (temp == NULL || temp->next == NULL)
-			return (NULL);
-		temp = temp->next;
+		if (i == idx - 1)
+		{
+			new_node->next = temp->next;
+			temp->next = new_node;
+			return (new_node);
+		}
+		else
+			temp = temp->next;
 	}
 
-	if (temp == NULL)
-		return (NULL);
-
-	if (new_node == NULL)
-		return (NULL);
-
-	new_node->n = n;
-	new_node->next = temp->next;
-	temp->next = new_node;
-
-	return (new_node);
+	return (NULL);
 }
